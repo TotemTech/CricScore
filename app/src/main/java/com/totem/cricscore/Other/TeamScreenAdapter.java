4c857a2d83189d81c6bdc78eb966965cdaf3624f
Umeshwaranm <totemtech2020@gmail.com>
2020-04-08 21:15:40 +0530

    Team - Recycler View

---
+++ app/src/main/java/com/totem/cricscore/Other/TeamScreenAdapter.java
@@ -0,0 +1,60 @@
+package com.totem.cricscore.Other;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.totem.cricscore.R;
+import com.totem.cricscore.model.Team;
+
+import java.util.List;
+
+public class TeamScreenAdapter extends RecyclerView.Adapter<TeamScreenAdapter.MyViewHolder> {
+
+    private List<Team> teamList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView teamName, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            teamName = (TextView) view.findViewById(R.id.teamName);
+
+        }
+    }
+
+    public TeamScreenAdapter(List<Team> teamList) {
+        this.teamList = teamList;
+    }
+
+    public TeamScreenAdapter() {
+    }
+
+
+    @NonNull
+    @Override
+    public TeamScreenAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Team team = teamList.get(position);
+        holder.teamName.setText(team.getTeamName());
+       /* holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());*/
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return teamList.size();
+    }
+}
--- app/src/main/java/com/totem/cricscore/TeamScreen.java
+++ app/src/main/java/com/totem/cricscore/TeamScreen.java
@@ -4,42 +4,50 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.totem.cricscore.Other.TeamScreenAdapter;
+import com.totem.cricscore.model.Team;
 
 import java.util.ArrayList;
 
 public class TeamScreen extends AppCompatActivity {
 
     private FloatingActionButton fabButton;
-    private ListView teamList;
-    ArrayAdapter<String> adapter;
-    ArrayList<String> listItems = new ArrayList<String>();
+    final ArrayList<Team> teamList = new ArrayList<Team>();
     private TextView teamName;
+    private RecyclerView recyclerView;
+    private TeamScreenAdapter teamAdapter;
+    Team team = new Team();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Team");
+        // Team team = new Team();
 
         setContentView(R.layout.activity_team_screen);
         fabButton = findViewById(R.id.fab_button);
-        teamList = findViewById(R.id.team_list);
+//        teamList = findViewById(R.id.team_list);
         teamName = findViewById(R.id.teamName);
 
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
+        recyclerView = (RecyclerView) findViewById(R.id.teamRecycler);
 
-
-//        adapter = new CustomListAdapter(this, teamList);
-        teamList.setAdapter(adapter);
+        teamAdapter = new TeamScreenAdapter(teamList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setAdapter(teamAdapter);
 
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,13 +58,16 @@ public class TeamScreen extends AppCompatActivity {
                 View layout = inflater.inflate(R.layout.prompts, null);
                 alert.setView(layout);
                 final EditText usernameInput = (EditText) layout.findViewById(R.id.prompt);
+
                 alert.setTitle("Add Team");
                 alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //listItems.add(usernameInput.getText().toString());
-                        adapter.add(usernameInput.getText().toString());
-                        adapter.notifyDataSetChanged();
+                        team.setTeamName(usernameInput.getText().toString());
+                        teamList.add(team);
+                        // adapter.add(usernameInput.getText().toString());
+                        teamAdapter.notifyDataSetChanged();
                     }
                 });
 
